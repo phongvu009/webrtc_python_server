e54de2e57f9aecf7a6599655733a586e65a673fc
Phong Vu <kimphong90@hotmail.com>
2024-07-21 20:03:08 -0500

    simple http connection

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Simple python server for Webrtc
+
+Sever will contain index.html
+Start server :
+```
+python3 server.py
+```
--- server.py
+++ server.py
@@ -11,7 +11,8 @@ print(ROOT)
 
 async  def index(request):
     #read content HTML in dir
-    html = open(os.path.join(ROOT,'index.html','r')).read()
+    html = open(os.path.join(ROOT,'index.html'),'r').read()
+    return web.Response(content_type='text/html',text=html)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
@@ -20,4 +21,4 @@ if __name__ == "__main__":
     #router
     app.router.add_get('/',index)
     #start server 
-    web.run_app(app, host='127.0.0.1', port=8081)
+    web.run_app(app, host='192.168.2.2', port=8081)
