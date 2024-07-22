6e8290a619348e5d203d5f9fb28604cab98f2f7f
Phong Vu <kimphong90@hotmail.com>
2024-07-22 11:29:55 -0500

    add ssl certificate and create client offer

--- server.py
+++ server.py
@@ -4,21 +4,41 @@ import os
 
 import sys 
 from aiohttp import web
+import ssl
+
 
 
 ROOT = os.path.dirname(__file__)
 print(ROOT)
 
+#Please modify ssl certificaiton path 
+cert_path = os.path.join(ROOT,'certification/cert.pem')
+cert_key_path = os.path.join(ROOT,'certification/key.pem')
+
+print(cert_path)
 async  def index(request):
     #read content HTML in dir
     html = open(os.path.join(ROOT,'index.html'),'r').read()
     return web.Response(content_type='text/html',text=html)
 
+
+def main():
+    app = web.Application()
+    
+    app.add_routes([
+        web.get('/',index)
+    ])
+
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(cert_path,cert_key_path)
+
+    web.run_app(app, host='192.168.2.2', port=8081, ssl_context=ssl_context)
+    # site = web.TCPSite(runner, 'localhost', 4443)
+    # await site.start()
+    print("Server running on https://192.168.2.2:8081")
+ 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    #create app instance
-    app = web.Application()
-    #router
-    app.router.add_get('/',index)
-    #start server 
-    web.run_app(app, host='192.168.2.2', port=8081)
+    # asyncio.run(main(), debug=True)
+    main()
